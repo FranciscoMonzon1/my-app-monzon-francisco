79958180a6f28ebaf7a70de3955ff4d96e1c7708
FranciscoMonzon <Franmonzon13@hotmil.com>
2023-07-10 23:24:53 -0300

    inicializando

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import Navbar from './componentes/Navbar';
 import ListContainer from './componentes/ItemListContainer';
-import ItemCount from './componentes/ItemListContainer';
+
 
 function App() {
 
   return <>
   <Navbar/>;
   <ListContainer
-    title="bienvenido a calculo a Mendoza Gaming"
-    subTitle="No te dejes ganar por culpa de accesorios"
+    title="Bienvenidos a Central Gym"
+    subTitle="Mucho mas que un gimnasio"
   />
-  {/* <ItemCount otrotitulo={otrotitulo} ValorInicial={0} stock={9}/> */}
   </>
 
 
--- src/componentes/Navbar/index.jsx
+++ src/componentes/Navbar/index.jsx
@@ -5,8 +5,8 @@ import CartWidget from '../CartWidget'
 const Navbar = () => {
   return (
     <div className='Navbar'>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Mendoza Gaming</a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light ">
+            <a className="navbar-brand" href="#"><img className='w-25' src="src/assets/images/Central.png" alt="" /></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -16,10 +16,10 @@ const Navbar = () => {
                   <a className="nav-link" href="#">inicio <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Productos</a>
+                  <a className="nav-link" href="#">Ubicacion</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Contacto</a>
+                  <a className="nav-link" href="#">Productos</a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link disabled" href="#"></a>
