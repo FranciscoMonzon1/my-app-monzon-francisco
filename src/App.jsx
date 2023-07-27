8fa6f29d6489373f3ff312ed8435141fac930428
FranciscoMonzon <Franmonzon13@hotmil.com>
2023-07-26 23:33:37 -0300

    fin entrega2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import Navbar from './componentes/Navbar';
-import ListContainer from './componentes/ItemListContainer';
-
-
-function App() {
-
-  return <>
-  <Navbar/>;
-  <ListContainer
-    title="Bienvenidos a Central Gym"
-    subTitle="Mucho mas que un gimnasio"
-  />
-  </>
-
-
+import React from 'react';
+import './app.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './componentes/Navbar/index';
+import Home from './pages/Home/Home';
+import Detail from './pages/Detail/Detail';
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:categoryId" element={<p>hola soy otro</p>} />
+        <Route path="/item/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
--- src/componentes/Navbar/index.jsx
+++ src/componentes/Navbar/index.jsx
@@ -1,12 +1,13 @@
-import './index.css'
+import '../Navbar/index.css'
 import React from 'react'
-import CartWidget from '../CartWidget'
+import CartWidget from '../CartWidget/index'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='Navbar'>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light ">
-            <a className="navbar-brand" href="#"><img className='w-25' src="src/assets/images/Central.png" alt="" /></a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Mendoza Gaming</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -16,10 +17,10 @@ const Navbar = () => {
                   <a className="nav-link" href="#">inicio <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Ubicacion</a>
+                  <a className="nav-link" href="#">Productos</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Productos</a>
+                  <a className="nav-link" href="#">Contacto</a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link disabled" href="#"></a>
